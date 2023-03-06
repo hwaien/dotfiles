89ec1c152b4507e8f70a8d36b9d816ec68053d45
hwaien <1613381+hwaien@users.noreply.github.com>
2023-03-05 22:07:30 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+git config --global commit.gpgsign true
+git config --global user.name "Hwai-En Ho"
+git config --global user.email dev@hwaien.com
+git config --global user.signingkey 6EFFD72D6523641B
